72721b4e9c27f5231b656b0b8de198fa6d0a976f
Rofhiwa Matumba <matumba@genesis.home>
2024-10-30 18:18:20 +0200

    Refurbished personal site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
